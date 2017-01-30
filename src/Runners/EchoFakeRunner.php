c746b1bbfa6911c430e928587bb63464243f02fe
Bukharov <sergey.bukharov@rtlabs.ru>
2017-01-30 12:21:22 +0300

    issue #20 https://github.com/adambrett/php-shell-wrapper/issues/20
    I suggest add FakeRunner which will be emulate running the command.
    It must be used in unit and integration tests.
    Also i suggest add EchoFakeRunner which will be not execute command but prints the command. It will be used when application runs with --dry-run argument

---
+++ src/Runners/EchoFakeRunner.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergey
+ * Date: 30.01.17
+ * Time: 12:07
+ */
+
+namespace Runners;
+
+
+use AdamBrett\ShellWrapper\Command\CommandInterface;
+use AdamBrett\ShellWrapper\Runners\FakeRunner;
+
+class EchoFakeRunner extends FakeRunner
+{
+    public function run(CommandInterface $command)
+    {
+        echo $command;
+        return parent::run($command);
+    }
+
+}
---
+++ tests/unit-tests/Runners/FakeRunnerTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergey
+ * Date: 30.01.17
+ * Time: 1:41
+ */
+
+namespace AdamBrett\ShellWrapper\Tests\Runners;
+
+
+use AdamBrett\ShellWrapper\Command;
+use AdamBrett\ShellWrapper\ExitCodes;
+use AdamBrett\ShellWrapper\Runners\FakeRunner;
+
+class FakeRunnerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testExecution()
+    {
+        $runner = new FakeRunner();
+        $returnedValue = $runner->run(new Command('ls'));
+
+        static::assertEquals(0, $returnedValue);
+        static::assertEquals('ls', $runner->getExecutedCommand());
+    }
+
+    public function testOutputAndError()
+    {
+        $outMsg = 'in processing...';
+        $errMsg = 'command terminated unexpectedly';
+        $runner = new FakeRunner(ExitCodes::USER_TERMINATED, $outMsg, $errMsg);
+
+        $runner->run(new Command('ls'));
+
+        static::assertEquals('ls', $runner->getExecutedCommand());
+        static::assertEquals($outMsg, $runner->getStandardOut());
+        static::assertEquals($errMsg, $runner->getStandardError());
+    }
+}
